ccb5791fb2207b438af89b0ef452baca9adf5bad
oshila123 <oshila.work@gmail.com>
2022-08-10 11:53:20 +0530

    change

--- src/components/Carousel/CarouselComponent.js
+++ src/components/Carousel/CarouselComponent.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Carousel } from 'react-bootstrap';
+import * as moment from 'moment'
 
 import reactImage from '../../assets/img/react.jpg';
 // import react2Image from '../../assets/img/react-2.jpg';
@@ -14,6 +15,8 @@ const CarouselComponent = () => {
         setIndex(selectedIndex);
     };
 
+    const expNow = moment().format('YYYY') - moment('2018').format('YYYY')
+
     return (
         <div className="slider">
             <Carousel activeIndex={index} onSelect={handleSelect} pause={false}>
@@ -26,6 +29,7 @@ const CarouselComponent = () => {
                     <Carousel.Caption>
                         <h3 className="content-overlay-topic mb-2">ReactJS</h3>
                         <p className="content-overlay">Collaborate with several commercial ReactJS projects</p>
+                        <p className="content-overlay d-none d-md-block">BYJU's, WhiteHatJr products</p>
                         <p className="content-overlay d-none d-md-block">Online Airticket and Hotel booking search engines</p>
                     </Carousel.Caption>
                 </Carousel.Item>
@@ -39,6 +43,7 @@ const CarouselComponent = () => {
                     <Carousel.Caption>
                         <h3 className="content-overlay-topic mb-2">Angular</h3>
                         <p className="content-overlay">Collaborate with several commercial Angular projects</p>
+                        <p className="content-overlay d-none d-md-block">getepic product</p>
                         <p className="content-overlay d-none d-md-block">Online Airticket and Hotel booking search engines</p>
                     </Carousel.Caption>
                 </Carousel.Item>
@@ -51,7 +56,7 @@ const CarouselComponent = () => {
 
                     <Carousel.Caption>
                         <h3 className="content-overlay-topic mb-2">Frontend Developer</h3>
-                        <p className="content-overlay d-none d-md-block">Over 2 years of industrial experince.</p>
+                        <p className="content-overlay d-none d-md-block">Over {expNow} years of industrial experince.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
